f964be5278000988b585bc1bdab699fe6bfef7ec
SakinaKothawala <89722949+SakinaKothawala@users.noreply.github.com>
2021-12-30 18:21:55 +0530

    Student App LAB 6

---
+++ SpringBootStudentAppLab/src/main/java/com/greatlearnings/springbootstudentapplab/SpringBootStudentAppLabApplication.java
@@ -0,0 +1,13 @@
+package com.greatlearnings.springbootstudentapplab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStudentAppLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStudentAppLabApplication.class, args);
+	}
+
+}
